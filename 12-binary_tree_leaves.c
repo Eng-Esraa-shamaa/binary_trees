2d61c77da1d4ddf73ded93a892740ed1afb69e9c
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-10-02 05:10:38 -0700

    e

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,19 +6,12 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t num;
+	size_t size = 0;
 
 	if (tree == NULL)
 		return (0);
-
-	while (tree)
-	{
-		if (tree->left == NULL && tree->right == NULL)
-		{
-			size +=1;
-		}
-		size += binary_tree_leaves(tree->left);
-		size += binary_tree_leaves(tree->right);
-	}
+	if ((tree->left == NULL) && (tree->right == NULL))
+		return (1);
+	size = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
 	return (size);
 }
