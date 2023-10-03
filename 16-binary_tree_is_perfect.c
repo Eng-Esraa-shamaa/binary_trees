b5813582903516695ff108fb6baaacd3dfd4cc89
salma elfadil <salmaelfadil@aucegypt.edu>
2023-10-03 12:00:23 -0700

    .

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,34 +8,36 @@
  */
 int is_perfect(const binary_tree_t *node, int d, int level)
 {
-	if (node == NULL)
-		return (0);
-	if (node->left == NULL && node->right == NULL)
-	{
-		if (d == level + 1)
-			return (1);
-	}
-	
-	if (node->left == NULL || node->right == NULL)
-		return (0);
-	
-	return ((is_perfect(node->left, d, level + 1) 
-				&& is_perfect(node->right, d, level + 1)) ? 1 : 0);
+        if (node == NULL)
+                return (0);
+        if (node->left == NULL && node->right == NULL)
+        {
+                if (d == level + 1)
+                        return (1);
+        }
+
+        if (node->left == NULL || node->right == NULL)
+                return (0);
+
+        return ((is_perfect(node->left, d, level + 1) 
+                                && is_perfect(node->right, d, level + 1)) ? 1 : 0);
 }
+
 /** binary_tree_d - measures the depth of a node in a binary tree.
  * @tree: pointer to the node to measure.
  * Return: int, 0 if tree is empty
  */
 int binary_tree_d(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	
-	int left_depth = binary_tree_d(tree->left);
-	int right_depth = binary_tree_d(tree->right);
-	
-	return (left_depth > right_depth) ? (left_depth + 1) : (right_depth + 1);
+        if (tree == NULL)
+                return (0);
+
+        int left_depth = binary_tree_d(tree->left);
+        int right_depth = binary_tree_d(tree->right);
+
+        return (left_depth > right_depth) ? (left_depth + 1) : (right_depth + 1);
 }
+
 /**
  * binary_tree_is_perfect -- checks if a binary tree is perfect
  * @tree: tree to be checked
@@ -46,3 +48,4 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
         int depth = binary_tree_d(tree);
         return (is_perfect(tree, depth, 0));
 }
+
